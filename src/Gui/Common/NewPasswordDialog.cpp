ec2e79fb852da1be6e48b0de21c111f2caa68ead
stevebrush <steevebrush@gmail.com>
2018-06-26 18:01:22 +0200

    Fix compilation under Qt 5.11.1

--- src/Gui/Common/NewPasswordDialog.cpp
+++ src/Gui/Common/NewPasswordDialog.cpp
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with Bytecoin.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <QStyle>
 #include "NewPasswordDialog.h"
 
 #include "ui_NewPasswordDialog.h"
